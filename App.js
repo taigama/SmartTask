6748a40d96b8162eda673818f53a5d6022fcdcab
Nguyễn Thanh Nam <tatanoi96@gmail.com>
2018-12-08 17:45:09 +0700

    2nd commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import WorkspaceScreen from './app/screens/WorkspaceScreen';
+import TaskDetailScreen from './app/screens/TaskDetailScreen';
+import ProjectScreen from './app/screens/ProjectScreen';
+import SplashScreen from './app/screens/SplashScreen';
+
+
+export const RootStack  = createStackNavigator({
+  Splash: { 
+    screen: SplashScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Splash',
+    }),
+  },
+  Project: { 
+    screen: ProjectScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Project',
+    }),
+  },
+  Workspace: { 
+    screen: WorkspaceScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Workspace',
+    }),
+  },
+  TaskDetail: { 
+    screen: TaskDetailScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'TaskDetail',
+    }),
+  },
+});
+
+export const App = createAppContainer(RootStack);
